6a5549da6dd481b749120a725afc732fd43799ab
alext <alextselegidis@gmail.com>
2017-06-11 13:01:27 +0200

    Added SVG minification task in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
-var gulp = require('gulp'); 
-var rename = require('gulp-rename'); 
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const svgmin = require('gulp-svgmin');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('css', function() {
-    var postcss    = require('gulp-postcss');
-    var sourcemaps = require('gulp-sourcemaps');
+gulp.task('svg', function () {
+    return gulp.src('images/*.svg')
+        .pipe(svgmin())
+        .pipe(gulp.dest('images/'));
+});
 
+gulp.task('css', function () {
     return gulp.src('styles.pcss')
         .pipe(sourcemaps.init())
-        .pipe(postcss([ 
-            require('autoprefixer'), 
+        .pipe(postcss([
+            require('autoprefixer'),
             require('precss'),
             require('postcss-color-function'),
             require('cssnano')
@@ -18,6 +24,6 @@ gulp.task('css', function() {
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('styles.pcss', ['css']);
 }); 
